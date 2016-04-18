39bb4fbdec01de8d0c186b3c62317fe82cae7622
Meg Davis <medavis732@gmail.com>
2016-04-18 00:10:33 +0000

    bad css fixes that are also bad

--- footer.php
+++ footer.php
@@ -1,3 +1,44 @@
+<section class="subscribe">
+   <div id="mc_embed_signup" class="container">
+   <form action="//megdavis.us13.list-manage.com/subscribe/post?u=d560d4bfecccc0880f0a11b6e&amp;id=347a11a800" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+       <div id="mc_embed_signup_scroll">
+   <div class="row">
+   <div class="four columns">
+   <h6>Subscribe to my mailing list</h6>
+   <p>Enter your email and you'll be notified every time I post.</p>
+  </div>
+
+  <div class="mc-field-group four columns">
+  <label for="mce-EMAIL">Your email</label>
+  <input type="email" value="" name="EMAIL" class="u-full-width" id="mce-EMAIL">
+  </div>
+
+  <div class="mc-field-group four columns">
+  <label for="mce-FNAME">First Name</label>
+  <input type="text" value="" name="FNAME" class="u-full-width" id="mce-FNAME">
+  </div>
+
+  </div>
+
+  <div id="mce-responses" class="clear">
+  <div class="response" id="mce-error-response" style="display:none"></div>
+  <div class="response" id="mce-success-response" style="display:none"></div>
+  </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d560d4bfecccc0880f0a11b6e_347a11a800" tabindex="-1" value=""></div>
+
+      <div class="row">
+<div class="four columns">
+<p></p>
+</div>
+      <div class="eight columns">
+      <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button-primary">
+      </div>
+      </div>
+  </form>
+  </div>
+
+
+</section>
 <section class="footer">
    <p>© <a href="http://megdavis.net">Meg Davis</a> 2016</p>
    </section>
